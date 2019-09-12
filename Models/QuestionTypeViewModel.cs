543d927daa8948af97a1fcc725d9611c8bc444c9
dek-haji <deeeqhaji@gmail.com>
2019-09-11 22:02:00 -0500

    now my url is going wrong URL

--- Models/QuestionTypeViewModel.cs
+++ Models/QuestionTypeViewModel.cs
@@ -9,9 +9,37 @@ namespace ASK_App.Models
     public class QuestionTypeViewModel
     {
         public List<Question> Questions { get; set; }
-        public SelectList QuestionTypes{ get; set; }
+        public SelectList QuestionTypes { get; set; }
         public string questionType { get; set; }
         public Question question { get; set; }
-        public string SearchString { get; set; }
+
+        public QuestionType QuestionType { get; set; }
+       
+        //public List<QuestionType> AvailableQuestion { get; set; }
+
+
+
+
+
+
+
+        // NOTE: Here we use an expression bodied, read-only property
+        //       AND the ?. operator
+        //       ...good times....
+        //public List<SelectListItem> AvailableOption
+        //{
+        //    get
+        //    {
+        //        if (AvailableQuestion == null)
+        //        {
+        //            return null;
+        //        }
+
+        //        var apt = AvailableQuestion?.Select(d => new SelectListItem(d.Name, d.QuestionTypeId.ToString())).ToList();
+        //        apt.Insert(0, new SelectListItem("Select Question", "Add Question"));
+
+        //        return apt;
+        //    }
+        //}
     }
 }
