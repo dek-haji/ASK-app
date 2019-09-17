113d8c2693f12d8db789f417cbda25cfffab65b8
dek-haji <deeeqhaji@gmail.com>
2019-09-16 20:38:27 -0500

    everything is working now

--- Controllers/QuestionsController.cs
+++ Controllers/QuestionsController.cs
@@ -27,7 +27,7 @@ namespace testDemo.Controllers
         // GET: Questions
         public async Task<IActionResult> Index(int? questionTypeId)
         {
-            var questions = _context.Question.Include(q => q.QuestionType).Include(q=> q.User).Include(q=> q.Answers).AsQueryable();
+            var questions = _context.Question.Include(q => q.QuestionType).Include(q=> q.User).Include(w=> w.Answers).AsQueryable();
                                         
            
             if (questionTypeId != null)
